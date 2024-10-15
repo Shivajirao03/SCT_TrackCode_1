00bf8c95b8204152175b57497d7d9b14e6f840bb
Shiva <119507197+Shivajirao03@users.noreply.github.com>
2024-10-15 21:12:45 +0530

    Update script.js

--- Rsponsive Loading Page/script.js
+++ Rsponsive Loading Page/script.js
@@ -1,18 +1,16 @@
 console.log("javascriptt is connected");
-// Toggle mobile menu
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 
 menuIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('show-menu'); // Toggle visibility
+  navLinks.classList.toggle('show-menu'); 
 });
 
-// Change navbar style on scroll
 window.addEventListener('scroll', () => {
   const navbar = document.getElementById('navbar');
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = 'rgba(40, 44, 52, 1)'; // Solid color on scroll
+    navbar.style.backgroundColor = 'rgba(40, 44, 52, 1)'; 
   } else {
-    navbar.style.backgroundColor = 'rgba(40, 44, 52, 0.9)'; // Semi-transparent
+    navbar.style.backgroundColor = 'rgba(40, 44, 52, 0.9)';
   }
 });
